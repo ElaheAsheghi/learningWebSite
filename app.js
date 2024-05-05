30e3b66170c567c4fc3496d952d072fccececdff
Elahe <asheghielahe@gmail.com>
2024-05-05 18:32:40 +0330

    set default middlewares

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 const express = require('express');
+const authRouter = require('./routes/v1/auth');
+const cors = require('cors');
+const path = require('path');
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(
+    '/courses/covers',
+    express.static(path.join(__dirname, "public", "courses", "covers"))
+);
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false })); //for uploading files
+app.use(bodyParser.json()); //for bodies with json type
+
+app.use("/v1/auth", authRouter);
+
 module.exports = app;
--- validators/register.js
+++ validators/register.js
@@ -3,11 +3,12 @@ const validator = require("fastest-validator");
 const validation = new validator();
 
 const schema = {
-    name: { type: "string", min: 3, max: 255},
-    username: { type: "string", min: 3, max: 100},
-    email: { type: "email", min: 10, max: 100},
-    password: { type: "string", min: 3, max: 24},
-    confirmPassword: { type: "equal", field: "password"},
+    name: { type: "string", min: 3, max: 255 },
+    username: { type: "string", min: 3, max: 100 },
+    email: { type: "email", min: 10, max: 100 },
+    phone: { type: "number", max: 11 },
+    password: { type: "string", min: 3, max: 24 },
+    confirmPassword: { type: "equal", field: "password" },
     $$strict: true,
 };
 
