f4d2baf4dd3bbee55bdc96000ffe8ed6d5aa3a3e
Elahe <asheghielahe@gmail.com>
2024-05-05 13:14:31 +0330

    add register validation schema

---
+++ validators/register.js
@@ -0,0 +1,16 @@
+const validator = require("fastest-validator");
+
+const validation = new validator();
+
+const schema = {
+    name: { type: "string", min: 3, max: 255},
+    username: { type: "string", min: 3, max: 100},
+    email: { type: "email", min: 10, max: 100},
+    password: { type: "string", min: 3, max: 24},
+    confirmPassword: { type: "equal", field: "password"},
+    $$strict: true,
+};
+
+const check = validation.compile(schema);
+
+module.exports = check;
